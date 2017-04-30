72be525dee3629b30d0d59fc7dd43b8bead65847
Adam <atomicpenguiens@gmail.com>
2017-04-30 15:28:29 +0100

    after refactor to check item is in list test

--- superlists/functional_tests_v2.py
+++ superlists/functional_tests_v2.py
@@ -6,12 +6,21 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):  
 
+    # remember, only methods that begin with test_ will get run as tests, so you
+    # can use other methods for your own purposes
     def setUp(self):  
         self.browser = webdriver.Chrome('c:/Users/adam/Desktop/chromedriver.exe')
 
     # will execute even if there is an error
     def tearDown(self):  
         self.browser.quit()
+
+    # helper method to check todo list contents
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
@@ -47,12 +56,20 @@ class NewVisitorTest(unittest.TestCase):
         #     "New to-do item did not appear in table. Contents were:\n{}".format(table.text)
         # )
         # replaces the above any function
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-             [row.text for row in rows]
-        )
+        # self.assertIn(
+        #     '2: Use peacock feathers to make a fly',
+        #      [row.text for row in rows]
+        # )
+
+        # above asserIn replaced with helper method
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        [...]
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
