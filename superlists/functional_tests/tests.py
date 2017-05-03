618cbe8414741eb9465af1c2b7b3196eb4865e5f
Adam <atomicpenguiens@gmail.com>
2017-05-03 19:47:26 +0100

    FT for page layout and styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -139,5 +139,26 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+
+    # tests that the css of the page has been applies
+    def test_layout_and_styling(self):
+            # Edith goes to the home page
+            self.browser.get(self.live_server_url)
+            self.browser.set_window_size(1024, 768)
+
+            # She notices the input box is nicely centered
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox.send_keys('testing')
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table('1: testing')
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                # margin of error
+                delta=10
+            )
+
+
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
