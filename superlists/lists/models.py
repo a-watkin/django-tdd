3873b8a3fac68b0f78668c5df758b774113d94d8
Adam <atomicpenguiens@gmail.com>
2017-05-09 19:26:50 +0100

    updated database location

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-
 class List(models.Model):
     pass
 
-    
-# Create your models here.
+
 class Item(models.Model):
-    # a new field means a new migration
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
-
-
--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -21,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'y*168yh^s2+r$)9v@swy%mebqz#u8*b%(i!m3@*o(ye*n5qk=h'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,7 +76,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
