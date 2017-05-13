516f4382237b093248432b744ce4b9a5bcdb14f3
Adam <atomicpenguiens@gmail.com>
2017-05-13 04:24:00 +0100

    moved fucntional tests to their own individual files

---
+++ superlists/functional_tests/base.py
@@ -0,0 +1,52 @@
+import os
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+# to run a single test file
+# python manage.py test functional_tests.test_list_item_validation
+
+MAX_WAIT = 10
+
+# you have to inherit from LiveServerTestCase
+class FunctionalTest(StaticLiveServerTestCase):
+
+    # remember, only methods that begin with test_ will get run as tests, so
+    # you can use other methods for your own purposes  
+    # 
+    # Do you remember I said
+    # that LiveServerTestCase had certain limitations? Well, one is that it
+    # always assumes you want to use its own test server, which it makes
+    # available at self.live_server_url.
+    def setUp(self):
+        self.browser = webdriver.Chrome('C:/Users/adam/Desktop/chromedriver.exe')
+        # staging server by default uses djangos dev server
+        # this changes that so that it looks for an environmental variable 
+        staging_server = os.environ.get('STAGING_SERVER') 
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server  
+
+    # will execute even if there is an error
+    def tearDown(self):
+        self.browser.refresh()
+        self.browser.quit()
+
+    # def check_for_row_in_list_table(self, row_text):
+    #     table = self.browser.find_element_by_id('id_list_table')
+    #     rows = table.find_elements_by_tag_name('tr')
+    #     self.assertIn(row_text, [row.text for row in rows])
+
+
+    # helper method to check todo list contents
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5)  
---
+++ superlists/functional_tests/test_layout_and_styling.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+    # tests that the css of the page has been applies
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            # margin of error
+            delta=10
+        )
+
+        
