36a577a6322cac84d7e0290a45f1ca5bc143caa3
Adam <atomicpenguiens@gmail.com>
2017-05-13 06:14:53 +0100

    Split out unit tests into two files

---
+++ superlists/lists/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+
+        # Django also gives us an API for querying the database via a class
+        # attribute, .objects, and we use the simplest possible query, .all(),
+        # which retrieves all the records for that table. The results are
+        # returned as a list-like object called a QuerySet, from which we can
+        # extract individual objects, and also call further functions, like
+        # .count().
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+
+        # compares list objects, behind the scenes they're tested with their ids
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
