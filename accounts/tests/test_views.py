c882241d4d2e7e0deff63b60fa765ea0586faed1
Adam <atomicpenguiens@gmail.com>
2017-09-07 02:45:41 +0100

    moved email test out of class LoginViewTest

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -74,18 +74,6 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(token.email, 'edith@example.com')
 
 
-
-
-
-
-class LoginViewTest(TestCase):
-
-    @patch('accounts.views.auth') 
-    def test_redirects_to_home_page(self, mock_auth):
-        response = self.client.get('/accounts/login?token=abcd123')
-        self.assertRedirects(response, '/')
-
-
     @patch('accounts.views.send_mail')
     def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
         self.client.post('/accounts/send_login_email', data={
@@ -95,6 +83,18 @@ class LoginViewTest(TestCase):
         expected_url = f'http://testserver/accounts/login?token={token.uid}'
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertIn(expected_url, body)
+
+
+
+# @patch('accounts.views.auth')
+class LoginViewTest(TestCase):
+
+    @patch('accounts.views.auth') 
+    def test_redirects_to_home_page(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(response, '/')
+
+
 
 
     @patch('accounts.views.auth')  
--- accounts/views.py
+++ accounts/views.py
@@ -7,13 +7,13 @@ from django.core.urlresolvers import reverse
 
 def send_login_email(request):
     email = request.POST['email']
-    print('this thing',type(send_mail))
+    # print('this thing',type(send_mail))
     token = Token.objects.create(email=email)
     # builds a URI, the is apparently the best way to do itfmd  #.request
     url = request.build_absolute_uri(  
         reverse('login') + '?token=' + str(token.uid)
     )
-    print(url)
+    # print(url)
     send_mail(
         'Your login link for Superlists',
         'Use this link to log in {}'.format(url),
@@ -29,8 +29,8 @@ def send_login_email(request):
 
 
 def redirect_after_login(request):
-    param1 = request.get_raw_uri()
-    print(param1)
+    # param1 = request.get_raw_uri()
+    # print(param1)
     return redirect('/')
 
 
