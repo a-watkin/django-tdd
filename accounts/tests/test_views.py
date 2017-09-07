1587012b17d96119ce92cc549c0f03a4b750c8de
Adam <atomicpenguiens@gmail.com>
2017-09-07 02:47:12 +0100

    moved mock decorator to be at class level, everything working

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -86,10 +86,10 @@ class SendLoginEmailViewTest(TestCase):
 
 
 
-# @patch('accounts.views.auth')
+@patch('accounts.views.auth')
 class LoginViewTest(TestCase):
 
-    @patch('accounts.views.auth') 
+    # @patch('accounts.views.auth') 
     def test_redirects_to_home_page(self, mock_auth):
         response = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(response, '/')
@@ -97,7 +97,7 @@ class LoginViewTest(TestCase):
 
 
 
-    @patch('accounts.views.auth')  
+    # @patch('accounts.views.auth')  
     def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):  
         self.client.get('/accounts/login?token=abcd123')
         self.assertEqual(
@@ -106,10 +106,16 @@ class LoginViewTest(TestCase):
         )
 
 
-    @patch('accounts.views.auth')  
+    # @patch('accounts.views.auth')  
     def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
         response = self.client.get('/accounts/login?token=abcd123')
         self.assertEqual(
             mock_auth.login.call_args,  
             call(response.wsgi_request, mock_auth.authenticate.return_value)  
         )
+
+    # @patch('accounts.views.auth')
+    # def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+    #     mock_auth.authenticate.return_value = None  
+    #     self.client.get('/accounts/login?token=abcd123')
+    #     self.assertEqual(mock_auth.login.called, False) 
