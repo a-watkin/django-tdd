a467ca9ba7870f1a0695723b72591deacbf3c300
Adam <atomicpenguiens@gmail.com>
2017-09-22 23:17:29 +0100

    updated functional tests with a helper method

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -36,6 +36,20 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.refresh()
         self.browser.quit()
 
+
+    def wait(fn):  
+        def modified_fn(*args, **kwargs):  
+            start_time = time.time()
+            while True:  
+                try:
+                    return fn(*args, **kwargs)  
+                except (AssertionError, WebDriverException) as e:  
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn  
+
+
     # def check_for_row_in_list_table(self, row_text):
     #     table = self.browser.find_element_by_id('id_list_table')
     #     rows = table.find_elements_by_tag_name('tr')
@@ -43,47 +57,79 @@ class FunctionalTest(StaticLiveServerTestCase):
 
 
     # helper method to check todo list contents
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:  
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return  
-            except (AssertionError, WebDriverException) as e:  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise e  
-                time.sleep(0.5)  
+    # def wait_for_row_in_list_table(self, row_text):
+    #     start_time = time.time()
+    #     while True:  
+    #         try:
+    #             table = self.browser.find_element_by_id('id_list_table')
+    #             rows = table.find_elements_by_tag_name('tr')
+    #             self.assertIn(row_text, [row.text for row in rows])
+    #             return  
+    #         except (AssertionError, WebDriverException) as e:  
+    #             if time.time() - start_time > MAX_WAIT:  
+    #                 raise e  
+    #             time.sleep(0.5)  
+
+
 
     # helper method that ensures that a function runs before selenium
     # looks for items on the page
     # expects to be passed a function
+
+    # This has been replaced here with a decorator function but it's still used
+    # in: test_login.py
+    # def wait_for(self, fn):
+    #     start_time = time.time()
+    #     while True:
+    #         try:
+    #             # calls the function that was passed to it
+    #             # returns its response
+    #             return fn()  
+    #         except (AssertionError, WebDriverException) as e:
+    #             if time.time() - start_time > MAX_WAIT:
+    #                 raise e
+    #             time.sleep(0.5)
+
+    # Does the same as above but uses the decorator.
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                # calls the function that was passed to it
-                # returns its response
-                return fn()  
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
+
+    # def wait_to_be_logged_in(self, email):
+    #     self.wait_for(
+    #         lambda: self.browser.find_element_by_link_text('Log out')
+    #     )
+    #     navbar = self.browser.find_element_by_css_selector('.navbar')
+    #     self.assertIn(email, navbar.text)
+
+
+    # def wait_to_be_logged_out(self, email):
+    #     self.wait_for(
+    #         lambda: self.browser.find_element_by_name('email')
+    #     )
+    #     navbar = self.browser.find_element_by_css_selector('.navbar')
+    #     self.assertNotIn(email, navbar.text)
 
 
 
+
+
+    @wait
+    def wait_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
 
+    @wait
     def wait_to_be_logged_out(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
