7ab180fcb3f110e63b3a9527433951e562db7a87
Adam <atomicpenguiens@gmail.com>
2017-09-30 00:12:00 +0100

    changed login testing behviour based on if code running on staging server

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -8,12 +8,16 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'elspeth@{host}'):
+    #  	Here’s the context manager that sets the host string, in the form user@server-address
+    # (I’ve hardcoded my server username, elspeth, so adjust as necessary).
+    with settings(host_string=f'awatkin@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'elspeth@{host}'):
+    with settings(host_string=f'awatkin@{host}'):
+        # Then, once we’re inside the context manager, we can just call
+        # Fabric commands as if we’re in a fabfile.
         session_key = run(f'{manage_dot_py} create_session {email}')
     return session_key.strip()
