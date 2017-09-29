54255a3405afdf2763e3aba5a4a0f5b11dfeedbe
Adam <atomicpenguiens@gmail.com>
2017-09-29 03:34:42 +0100

    added server_tools.py and made changes to base.py

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,11 +1,13 @@
 import os
-import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+import time
+from .server_tools import reset_database
 
 # to run a single test file:
 # python manage.py test functional_tests.test_list_item_validation
+
 
 MAX_WAIT = 10
 print('base called')
@@ -18,8 +20,8 @@ class FunctionalTest(StaticLiveServerTestCase):
         return self.browser.find_element_by_id('id_text')
 
     # remember, only methods that begin with test_ will get run as tests, so
-    # you can use other methods for your own purposes  
-    # 
+    # you can use other methods for your own purposes
+    #
     # Do you remember I said
     # that LiveServerTestCase had certain limitations? Well, one is that it
     # always assumes you want to use its own test server, which it makes
@@ -49,17 +51,17 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.quit()
 
 
-    def wait(fn):  
-        def modified_fn(*args, **kwargs):  
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
             start_time = time.time()
-            while True:  
+            while True:
                 try:
-                    return fn(*args, **kwargs)  
-                except (AssertionError, WebDriverException) as e:  
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
                     if time.time() - start_time > MAX_WAIT:
                         raise e
                     time.sleep(0.5)
-        return modified_fn  
+        return modified_fn
 
 
     # def check_for_row_in_list_table(self, row_text):
@@ -71,16 +73,16 @@ class FunctionalTest(StaticLiveServerTestCase):
     # helper method to check todo list contents
     # def wait_for_row_in_list_table(self, row_text):
     #     start_time = time.time()
-    #     while True:  
+    #     while True:
     #         try:
     #             table = self.browser.find_element_by_id('id_list_table')
     #             rows = table.find_elements_by_tag_name('tr')
     #             self.assertIn(row_text, [row.text for row in rows])
-    #             return  
-    #         except (AssertionError, WebDriverException) as e:  
-    #             if time.time() - start_time > MAX_WAIT:  
-    #                 raise e  
-    #             time.sleep(0.5)  
+    #             return
+    #         except (AssertionError, WebDriverException) as e:
+    #             if time.time() - start_time > MAX_WAIT:
+    #                 raise e
+    #             time.sleep(0.5)
 
 
 
@@ -96,7 +98,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     #         try:
     #             # calls the function that was passed to it
     #             # returns its response
-    #             return fn()  
+    #             return fn()
     #         except (AssertionError, WebDriverException) as e:
     #             if time.time() - start_time > MAX_WAIT:
     #                 raise e
