6393edad11a1c2efe848cbc16409cfb6ca077b6e
Adam <atomicpenguiens@gmail.com>
2017-09-30 01:28:36 +0100

    troubleshooting why the test isn't getting the environment variable for the email password

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -10,7 +10,7 @@ from .base import FunctionalTest
 # TEST_EMAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
-
+print('why?', os.environ['EMAIL_PASSWORD'])
 
 class LoginTest(FunctionalTest):
 
