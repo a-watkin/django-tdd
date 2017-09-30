4299758709224521d5af3f099bf66dfca737fbfc
Adam <atomicpenguiens@gmail.com>
2017-09-30 01:29:27 +0100

    troubleshooting why the test isn't getting the environment variable for the email password

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -51,7 +51,7 @@ class LoginTest(FunctionalTest):
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
         if self.staging_server:
-            test_email = 'edith.testuser@gmail.com'
+            test_email = 'test+atomicpenguines@gmail.com'
         else:
             test_email = 'edith@example.com'
 
